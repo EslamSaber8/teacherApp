580f6b43f544b6f0506fa013bda52300e7bf9a03
EslamSaber8 <56899775+EslamSaber8@users.noreply.github.com>
2023-11-14 01:34:05 +0200

    Update cartService.jslast

--- services/cartService.js
+++ services/cartService.js
@@ -192,14 +192,15 @@ const createCard = async (session) => {
 // @route   POST /webhook-checkout
 // @access  Protected/User
 exports.webhookCheckout = asyncHandler(async (req, res, next) => {
-  const sig = req.headers[process.env.STRIPE_WEBHOOK_SECRET];
+  const sig = req.headers['stripe-signature'];
 
   let event;
 
   try {
     event = stripe.webhooks.constructEvent(
       req.body,
-    const sig = req.headers['stripe-signature'];
+      sig,
+      process.env.STRIPE_WEBHOOK_SECRET
     );
   } catch (err) {
     return res.status(400).send(`Webhook Error: ${err.message}`);
